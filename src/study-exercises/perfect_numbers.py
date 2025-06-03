fd88c9b4ed83e64406401caa7b2e08e0e2fcffa5
AmandaEudes <amanda_eudes@hotmail.com>
2025-06-03 15:08:19 -0300

    Corrections

--- src/study-exercises/perfect_numbers.py
+++ src/study-exercises/perfect_numbers.py
@@ -28,23 +28,23 @@
 # your language track, you may also need to implement a way to determine whether
 # a given number is abundant or deficient.
 
-def sum_of_divisors(n):
+def sum_of_divisors(n: int) -> int:
     divisors_sum = 0
     for i in range(1, n // 2 + 1):
         if n % i == 0:
             divisors_sum += i
     return divisors_sum
 
-def classify_number(n):
+def classify_number(n: int) -> str:
     divisors_sum = sum_of_divisors(n)
 
     if divisors_sum == n:
         return "Perfect"
-    elif divisors_sum > n:
+    if divisors_sum > n:
         return "Abundant"
-    else:
-        return "Deficient"
 
-number = 30
+    return "Deficient"
+
+number = 8
 classification = classify_number(number)
 print(f"The number {number} is {classification}.")
