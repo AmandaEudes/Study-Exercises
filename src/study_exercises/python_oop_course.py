7244a2c19e1973d5c714941f463f40a4c529c4dd
AmandaEudes <amanda_eudes@hotmail.com>
2025-07-07 09:22:50 -0300

    Starting oop course of udemy

---
+++ src/study_exercises/python_oop_course.py
@@ -0,0 +1,54 @@
+# name = "Amanda"
+# age = 34
+
+# print(type(name))
+# print(name.upper())
+# print(type(age))
+
+
+# class Dog:
+#     def __init__(self, name, breed, owner):
+#         self.name = name
+#         self.breed = breed
+#         self.owner = owner
+
+#     def bark(self):
+#         print("Whoof whoof")
+
+
+# class Owner:
+#     def __init__(self, name, address, contact_number):
+#         self.name = name
+#         self.address = address
+#         self.phone_number = contact_number
+
+
+# owner1 = Owner("Danny", "122 Springfield Drive", "888-999")
+# dog1 = Dog("Bruce", "Scottish Terrier", owner1)
+# dog1.bark()
+# print(dog1.name)
+# print(dog1.breed)
+# print(dog1.owner.name)
+
+# owner2 = Owner("Sally", "122 Springfield Drive", "888-999")
+# dog2 = Dog("Freya", "Greyhound", owner2)
+# dog2.bark()
+# print(dog2.name)
+# print(dog2.breed)
+# print(dog2.owner.name)
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def greet(self):
+#         print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+
+# person1 = Person("Alice", 30)
+# person1.greet()
+
+# person2 = Person("Bob", 42)
+# person2.greet()
